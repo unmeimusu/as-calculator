f0c38679aee346127db9b9e6a5fd9306dda6f81d
unmeimusu <unmeimusu@gmail.com>
2023-05-10 11:04:24 +0700

    Update files

--- app/src/components/Calculator.svelte
+++ app/src/components/Calculator.svelte
@@ -1,87 +1,86 @@
 <script>
-    let inputValue = '';
-    let currentOperator = '';
-    let result = 0;
-  
-    $: {
-      if (inputValue !== '') {
-        if (currentOperator === '+') {
-          result += parseFloat(inputValue);
-        } else if (currentOperator === '-') {
-          result -= parseFloat(inputValue);
-        } else if (currentOperator === '*') {
-          result *= parseFloat(inputValue);
-        } else if (currentOperator === '/') {
-          result /= parseFloat(inputValue);
-        } else {
-          result = parseFloat(inputValue);
-        }
-        inputValue = '';
+  let inputValue = '';
+  let currentOperator = '';
+  let result = 0;
+
+  $: {
+    if (inputValue !== '') {
+      if (currentOperator === '+') {
+        result += parseFloat(inputValue);
+      } else if (currentOperator === '-') {
+        result -= parseFloat(inputValue);
+      } else if (currentOperator === '*') {
+        result *= parseFloat(inputValue);
+      } else if (currentOperator === '/') {
+        result /= parseFloat(inputValue);
+      } else {
+        result = parseFloat(inputValue);
       }
+      inputValue = '';
     }
-  
-    function handleNumberClick(num) {
-      inputValue += num;
-    }
-  
-    function handleOperatorClick(operator) {
-      currentOperator = operator;
-    }
-  
-    function handleEqualClick() {
-      if (inputValue !== '') {
-        if (currentOperator === '+') {
-          result += parseFloat(inputValue);
-        } else if (currentOperator === '-') {
-          result -= parseFloat(inputValue);
-        } else if (currentOperator === '*') {
-          result *= parseFloat(inputValue);
-        } else if (currentOperator === '/') {
-          result /= parseFloat(inputValue);
-        } else {
-          result = parseFloat(inputValue);
-        }
-        inputValue = '';
-        currentOperator = '';
+  }
+
+  function handleNumberClick(num) {
+    inputValue += num;
+  }
+
+  function handleOperatorClick(operator) {
+    currentOperator = operator;
+  }
+
+  function handleEqualClick() {
+    if (inputValue !== '') {
+      if (currentOperator === '+') {
+        result += parseFloat(inputValue);
+      } else if (currentOperator === '-') {
+        result -= parseFloat(inputValue);
+      } else if (currentOperator === '*') {
+        result *= parseFloat(inputValue);
+      } else if (currentOperator === '/') {
+        result /= parseFloat(inputValue);
+      } else {
+        result = parseFloat(inputValue);
       }
-    }
-  
-    function handleClearClick() {
       inputValue = '';
       currentOperator = '';
-      result = 0;
     }
-  </script>
-  
+  }
+
+  function handleClearClick() {
+    inputValue = '';
+    currentOperator = '';
+    result = 0;
+  }
+</script>
+
+<div>
+  <input type="text" bind:value={result} disabled>
+
   <div>
-    <input type="text" bind:value={result} disabled>
-  
-    <div>
-      <button on:click={() => handleNumberClick('1')}>1</button>
-      <button on:click={() => handleNumberClick('2')}>2</button>
-      <button on:click={() => handleNumberClick('3')}>3</button>
-      <button on:click={() => handleOperatorClick('+')}>+</button>
-    </div>
-  
-    <div>
-      <button on:click={() => handleNumberClick('4')}>4</button>
-      <button on:click={() => handleNumberClick('5')}>5</button>
-      <button on:click={() => handleNumberClick('6')}>6</button>
-      <button on:click={() => handleOperatorClick('-')}>-</button>
-    </div>
-  
-    <div>
-      <button on:click={() => handleNumberClick('7')}>7</button>
-      <button on:click={() => handleNumberClick('8')}>8</button>
-      <button on:click={() => handleNumberClick('9')}>9</button>
-      <button on:click={() => handleOperatorClick('*')}>*</button>
-    </div>
-  
-    <div>
-      <button on:click={() => handleClearClick()}>C</button>
-      <button on:click={() => handleNumberClick('0')}>0</button>
-      <button on:click={() => handleEqualClick()}>=</button>
-      <button on:click={() => handleOperatorClick('/')}>/</button>
-    </div>
+    <button on:click={() => handleNumberClick('1')}>1</button>
+    <button on:click={() => handleNumberClick('2')}>2</button>
+    <button on:click={() => handleNumberClick('3')}>3</button>
+    <button on:click={() => handleOperatorClick('+')}>+</button>
   </div>
-  
+
+  <div>
+    <button on:click={() => handleNumberClick('4')}>4</button>
+    <button on:click={() => handleNumberClick('5')}>5</button>
+    <button on:click={() => handleNumberClick('6')}>6</button>
+    <button on:click={() => handleOperatorClick('-')}>-</button>
+  </div>
+
+  <div>
+    <button on:click={() => handleNumberClick('7')}>7</button>
+    <button on:click={() => handleNumberClick('8')}>8</button>
+    <button on:click={() => handleNumberClick('9')}>9</button>
+    <button on:click={() => handleOperatorClick('*')}>*</button>
+  </div>
+
+  <div>
+    <button on:click={() => handleClearClick()}>C</button>
+    <button on:click={() => handleNumberClick('0')}>0</button>
+    <button on:click={() => handleEqualClick()}>=</button>
+    <button on:click={() => handleOperatorClick('/')}>/</button>
+  </div>
+</div>
